74e6c0285c0836514b8e5236b7935d14fe70e78b
Abou Salmane <djibrilndye@gmail.com>
2020-10-11 18:39:30 +0000

    authencation OK

--- functions/getword/getword.js
+++ functions/getword/getword.js
@@ -4,15 +4,24 @@ exports.handler = async (event, context,callback) => {
   try {
     const pwd = event.queryStringParameters.pwd;
     console.log("Mot de Passe "+pwd);
+  if(pwd=="Qamar@221"){
     callback(null, {
       statusCode: 200,
       header: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({message:"ok"}),
+    });
+  }
+  else{
+    callback(null, {
+      statusCode: 403,
+      header: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({"message": "Hello"}),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
+      body: JSON.stringify({"message": "Access Forbidden"}),
     });
+  }
   } catch (err) {
     return { statusCode: 500, body: err.toString() }
   }
